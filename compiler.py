1185a7d376e0fccbb89265440f2402657b9d4af8
Umutambyi Gad <umutambyig@gmail.com>
2020-12-13 13:32:37 +0100

    supported whitespace and dots before specifying directory or  static file

--- compiler.py
+++ compiler.py
@@ -2,13 +2,13 @@ import re
 
 
 # compile for href with double quotes ex. href=""
-regex_for_href_with_double_quotes = re.compile(r'\b(href=")(\w.+?)\"\B')
+regex_for_href_with_double_quotes = re.compile(r'\b(href=")(\.{0,}\/{0,}\s{0,}\w.+?)\"\B')
 
 # compile for src with double quotes ex. src=""
-regex_for_src_with_double_quotes = re.compile(r'\b(src=")(\w.+?)\"\B')
+regex_for_src_with_double_quotes = re.compile(r'\b(src=")(\.{0,}\/{0,}\s{0,}\w.+?)\"\B')
 
 # compile for href with single quotes ex. href=''
-regex_for_href_with_single_quotes = re.compile(r"\b(href=')(\w.+?)\'\B")
+regex_for_href_with_single_quotes = re.compile(r"\b(href=')(\.{0,}\/{0,}\s{0,}\w.+?)\'\B")
 
 # compile for src with single quotes ex. src=''
-regex_for_src_with_single_quotes = re.compile(r"\b(src=')(\w.+?)\'\B")
+regex_for_src_with_single_quotes = re.compile(r"\b(src=')(\.{0,}\/{0,}\s{0,}\w.+?)\'\B")
