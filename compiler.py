1a7adbf7da529eaa3832c5c899b6825c429f58a8
Umutambyi Gad <umutambyig@gmail.com>
2021-02-23 19:53:44 +0100

    Added regex for finding images path embed in url

--- compiler.py
+++ compiler.py
@@ -12,3 +12,6 @@ regex_for_href_with_single_quotes = re.compile(r"\b(href=')(\.{0,}\/{0,}\s{0,}\w
 
 # compile for src with single quotes ex. src=''
 regex_for_src_with_single_quotes = re.compile(r"\b(src=')(\.{0,}\/{0,}\s{0,}\w.+?)\'\B")
+
+# compile for url which embedded in html style tag ex. style="url()"
+regex_for_url = re.compile(r'\b(style=")(\.{0,}\/{0,}\s{0,}\w.+?)\"\B')
