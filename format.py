5b25e00a8429c1d8ccefe696f2439f3c2e88dd0d
Umutambyi Gad <umutambyig@gmail.com>
2021-02-23 19:54:20 +0100

    Implemented regex for finding images path embed in url

--- format.py
+++ format.py
@@ -3,7 +3,7 @@ from compiler import regex_for_href_with_double_quotes as d_href
 from compiler import regex_for_src_with_double_quotes as d_src
 from compiler import regex_for_href_with_single_quotes as s_href
 from compiler import regex_for_src_with_single_quotes as s_src
-
+from compiler import regex_for_url
 
 with open('fileToFormat.txt', 'r', encoding="utf8") as file:
 	htmlFiles = file.read().strip().split('\n')
@@ -41,11 +41,26 @@ def formating(htmlFile):
 				single.append(i.group(2).strip())
 
 
+		# finding all style:...url() and format it if it's needed
+		for x in re.finditer(regex_for_url, contents):
+			pick_url  = re.compile(r'\b(:\s*?url\()(.*)\)\B')
+			for y in re.finditer(pick_url, x.group(2)):
+				if y.group(2).startswith("'"):
+					if not y.group(2)[1:-1].startswith('http'):
+						# just to check if staticfile is not in the list to avoid duplicate
+						if y.group(2)[1:-1] not in (single, double):
+							contents = re.sub(y.group(2), "{% static " + y.group(2) + " %}", contents)
+				else:
+					if not y.group(2)[1:-1].startswith('http'):
+						double.append(y.group(2).strip())
+
+
 		# replacing the all double quoted matches
 		# [*{* double }] to avoid duplicates
 		double = [*{*double}]
 		for i in range(len(double)):
 			contents = re.sub(double[i], r"{% static '" + double[i] + r"' %}", contents)
+
 
 		# replacing the all single quoted matches
 		# [*{* single }] to avoid duplicates
